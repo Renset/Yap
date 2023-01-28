4a0fb80522c724113309396c6ea58f7986dba677
Renat Notfullin <renat@notfullin.com>
2023-01-28 17:35:12 +0300

    Initial Commit

---
+++ Yap/YapApp.swift
@@ -0,0 +1,17 @@
+//
+//  YapApp.swift
+//  Yap
+//
+//  Created by Renat Notfullin on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct YapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
